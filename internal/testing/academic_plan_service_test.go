918adeafe1c0088b414be8c0ff92c10954349b4b
iki-rumondor <ikirumondor07@gmail.com>
2024-03-12 21:39:21 +0800

    feat: melengkapi unit test untuk create academic plan

--- internal/testing/academic_plan_service_test.go
+++ internal/testing/academic_plan_service_test.go
@@ -1,6 +1,7 @@
 package testing
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/iki-rumondor/go-monev/internal/http/request"
@@ -32,6 +33,26 @@ func TestCreateAcademicPlan_Success(t *testing.T) {
 	err := academicPlanService.CreateAcademicPlan("user_uuid", &req)
 	assert.NoError(t, err)
 }
+func TestCreateAcademicPlan_Failed(t *testing.T) {
+	mockRepo := &mocks.AcademicPlanRepoInterface{}
+	mockRepo.On("FindSubjectBy", mock.Anything, mock.Anything).Return(&models.Subject{}, nil)
+	mockRepo.On("FindAcademicYearBy", mock.Anything, mock.Anything).Return(&models.AcademicYear{}, nil)
+	mockRepo.On("CreateAcademicPlan", mock.Anything).Return(errors.New("something error"))
+
+	academicPlanService := services.NewAcademicPlanService(mockRepo)
+
+	available := true
+
+	req := request.AcademicPlan{
+		SubjectUuid:      "subject_uuid",
+		AcademicYearUuid: "academic_year_uuid",
+		Available:        &available,
+		Note:             "test note",
+	}
+
+	err := academicPlanService.CreateAcademicPlan("user_uuid", &req)
+	assert.Error(t, err)
+}
 
 func TestCreateAcademicPlan_SubjectNotFound(t *testing.T) {
 	mockRepo := &mocks.AcademicPlanRepoInterface{}
@@ -52,4 +73,25 @@ func TestCreateAcademicPlan_SubjectNotFound(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.Equal(t, "404: Mata Kuliah Tidak Ditemukan", err.Error())
+}
+func TestCreateAcademicPlan_AcademicYearNotFound(t *testing.T) {
+	mockRepo := &mocks.AcademicPlanRepoInterface{}
+	mockRepo.On("FindSubjectBy", mock.Anything, mock.Anything).Return(&models.Subject{}, nil)
+	mockRepo.On("FindAcademicYearBy", mock.Anything, mock.Anything).Return(nil, gorm.ErrRecordNotFound)
+
+	academicPlanService := services.NewAcademicPlanService(mockRepo)
+
+	available := true
+
+	req := &request.AcademicPlan{
+		SubjectUuid:      "subject_uuid",
+		AcademicYearUuid: "academic_year_uuid",
+		Available:        &available,
+		Note:             "test note",
+	}
+
+	err := academicPlanService.CreateAcademicPlan("user_uuid", req)
+
+	assert.Error(t, err)
+	assert.Equal(t, "404: Tahun Ajaran Tidak Ditemukan", err.Error())
 }
