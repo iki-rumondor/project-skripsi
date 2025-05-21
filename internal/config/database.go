40eddce73537603b484965163a1c66795c7e4c59
iki-rumondor <ikirumondor07@gmail.com>
2025-05-21 13:48:06 +0800

    add automation for creating table if doesn't exist

--- internal/config/database.go
+++ internal/config/database.go
@@ -20,6 +20,22 @@ func NewMysqlDB() (*gorm.DB, error) {
 	dbPort := os.Getenv("DBPORT")
 	dbName := os.Getenv("DBNAME")
 
+	initialDSN := fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8mb4&parseTime=True&loc=Local&timeout=5s", dbUser, dbPassword, dbHost, dbPort)
+	initialDB, err := gorm.Open(mysql.Open(initialDSN), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	var exists bool
+	query := fmt.Sprintf("SELECT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '%s')", dbName)
+	initialDB.Raw(query).Scan(&exists)
+
+	if !exists {
+		if err := initialDB.Exec("CREATE DATABASE " + dbName).Error; err != nil {
+			return nil, err
+		}
+	}
+	
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
 	gormDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
